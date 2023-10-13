ab2c27862509bd81a1c177935581d3b4fc2356fd
haydarnt8 <haydarnt8@gmail.com>
2023-10-14 02:28:54 +0300

    first uploud

---
+++ components/Color.vue
@@ -0,0 +1,28 @@
+<template>
+    <div v-if="colors.length !== 0" class="h-full flex items-center -space-x-2 overflow-hidden">
+        <div v-if="colors.length > 0" class="inline-block h-5 w-5 rounded-full ring-2 ring-white"
+            :style="{ backgroundColor: colors[0] }">
+        </div>
+        <div v-if="colors.length > 1" class="inline-block h-5 w-5 rounded-full ring-2 ring-white"
+            :style="{ backgroundColor: colors[1] }"></div>
+        <div v-if="colors.length > 2 && colors.length < 4" class="inline-block h-5 w-5 rounded-full ring-2 ring-white"
+            :style="{ backgroundColor: colors[2] }"></div>
+        <div v-if="colors.length > 3" class="inline-block h-5 w-5 rounded-full ring-2 ring-white bg-slate-400">
+            <div class="flex justify-center items-center h-full text-white text-xs">
+                +{{ colors.length - 2 }}
+            </div>
+            >
+
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+const { colors } = defineProps({
+    colors: Array
+})
+console.log(colors.length)
+
+
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+    <main>
+        <div class="h-[30vh] flex flex-col gap-7 justify-center items-center">
+            <h1 class="text-5xl font-bold" >Miswag New Arrivals</h1>
+            <p>
+                Stay Ahead of the Curve with Miswag's Fresh Arrivals!
+            </p>
+        </div>
+        <slot />
+    </main>
+</template>
