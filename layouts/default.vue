ae8ef1f6228d5219eee528ff08aa25ed161ae2b1
haydarnt8 <haydarnt8@gmail.com>
2023-10-14 14:55:25 +0300

    xx

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <main class="flex flex-col justify-center bg-slate-100">
+    <main class="flex flex-col justify-center">
         <div class="h-[30vh]  flex flex-col gap-7 justify-center items-center">
             <h1 class="text-5xl font-bold">Miswag New Arrivals</h1>
             <p>
@@ -9,11 +9,20 @@
         <CategoryNav />
         <slot />
 
+        <button @click="show = !show" class="bg-black text-white p-3">
+            show
+        </button>
+
+        <div v-if="show" v-html="adiv"></div>
+
+
     </main>
 </template>
 
 <script setup>
 const router = useRouter()
+const show = ref(false)
+const adiv = "<div class='h-20 w-screen bg-red-500'></div>"
 
 onMounted(() => {
     // console.log('mounted')
